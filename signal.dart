2c0db1f905cae3e108cf28eb8974f649e2adcaa7
jodeveloper8 <jodeveloper8@gmail.com>
2024-07-10 08:06:21 +0000

    explor basic of signals in dart

---
+++ signal.dart
@@ -0,0 +1,48 @@
+Function? _subscriber = null;
+
+class Signal {
+  final subscribers = <Function>[];
+  dynamic _state;
+  Signal(value) {
+    _state = value;
+  }
+
+  int get value {
+    if (_subscriber != null) {
+      subscribers.add(_subscriber!);
+    }
+    return _state;
+  }
+
+  set value(var val) {
+    _state = val;
+    subscribers.forEach((sub) => sub.call());
+  }
+
+  Function effect(Function callback) {
+    _subscriber = callback;
+    callback.call();
+    _subscriber = null;
+
+    return () => {
+      subscribers.remove(callback)
+    };
+  }
+}
+
+void main() {
+  final variable = Signal(0);
+  
+  
+  
+  final unsubscribe = variable.effect(()=>{
+    print('count is ${variable.value}')
+  });
+
+  variable.value = 5;
+  
+  variable.value = 25;
+  unsubscribe.call();
+  variable.value = 35;
+  
+}
