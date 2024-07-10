a4b93eee86e3748da1551faf9979b60502097c5d
jodeveloper8 <jodeveloper8@gmail.com>
2024-07-10 08:15:21 +0000

    wip

--- signal.dart
+++ signal.dart
@@ -1,20 +1,20 @@
 Function? _subscriber = null;
 
-class Signal {
+class Signal<T> {
   final subscribers = <Function>{};
   dynamic _state;
   Signal(value) {
     _state = value;
   }
 
-  int get value {
+  T get value {
     if (_subscriber != null) {
       subscribers.add(_subscriber!);
     }
     return _state;
   }
 
-  set value(var val) {
+  set value(T val) {
     _state = val;
     subscribers.forEach((sub) => sub());
   }
@@ -24,25 +24,27 @@ class Signal {
     callback();
     _subscriber = null;
 
-    return () => {
-      subscribers.remove(callback)
-    };
+    return () => {subscribers.remove(callback)};
   }
 }
 
 void main() {
-  final variable = Signal(0);
-  
-  
-  
-  final unsubscribe = variable.effect(()=>{
-    print('count is ${variable.value}')
+  final variable = Signal<int>(0);
+
+  final unsubscribe =
+      variable.effect(() => {print('count is ${variable.value}')});
+
+  variable.effect(() {
+    print(variable.value * 2);
+  });
+
+  variable.effect(() {
+    print(variable.value * 3);
   });
 
   variable.value = 5;
-  
+
   variable.value = 25;
   unsubscribe();
   variable.value = 35;
-
 }
